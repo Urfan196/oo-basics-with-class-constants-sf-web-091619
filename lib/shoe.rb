c52a4be28d3e44e6189621f688c3f2d1c61a66b5
Urfan Guliyev <urfan196@yahoo.com>
2019-09-19 01:50:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,11 +6,12 @@ BRANDS=[]
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def brand=(brand)
   @brand = brand
-  BRANDS << brand unless BRANDS.include?(brand)
+
   end
 
   def cobble
