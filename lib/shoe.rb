92232ce1812b4a8e320de8d8802d08e641240ff1
Urfan Guliyev <urfan196@yahoo.com>
2019-09-19 01:38:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-@@BRANDS=[]
+BRANDS=[]
 
   def initialize(brand)
     @brand = brand
@@ -10,7 +10,7 @@ class Shoe
 
   def brand=(brand)
   @brand = brand
-  @@BRANDS.uniq << brand
+  BRANDS << brand.uniq
   end
 
   def cobble
